22e536c166ac85ef13e6ccca3af01790e53fde67
heojongwoo010 <jongu010@gmail.com>
2025-03-14 15:07:33 +0900

    Add PythonStudy directory

---
+++ PythonStudy/Programmers/기초/day1/문자열 반복 출력.py
@@ -0,0 +1,3 @@
+str, n = map(int, input().split(' '))
+for i in range(n):
+    print(str)
---
+++ PythonStudy/Softeer/HSAT/Lv3/gradeAvg.py
@@ -0,0 +1,13 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+
+scores = list(map(int, sys.stdin.readline().split()))
+
+print(type(scores))
+
+
+# for i in range(0, K):
+#     A, B = map(int, sys.stdin.readline().split())
+#     for j in range(A, B+1):
+        
